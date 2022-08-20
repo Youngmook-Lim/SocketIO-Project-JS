b665d66880c4fe32778f4653536acec0886a0231
Youngmook Lim <youngmookk@gmail.com>
2022-08-20 13:49:09 +0900

    Add data channel functionality

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,6 +13,7 @@ let muted = false;
 let cameraOff = false;
 let roomName;
 let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
   try {
@@ -119,6 +120,9 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 // Socket code
 
 socket.on("welcome", async () => {
+  myDataChannel = myPeerConnection.createDataChannel("chat");
+  myDataChannel.addEventListener("message", (e) => console.log(e.data));
+  console.log("made data channel");
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
   console.log("sent the offer");
@@ -126,6 +130,10 @@ socket.on("welcome", async () => {
 });
 
 socket.on("offer", async (offer) => {
+  myPeerConnection.addEventListener("datachannel", (e) => {
+    myDataChannel = e.channel;
+    myDataChannel.addEventListener("message", (e) => console.log(e.data));
+  });
   console.log("received the offer");
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
